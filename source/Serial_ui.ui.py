ac1be2bb1780d332e3bba402105aec12a88f587b
DS <cckn1985@gmail.com>
2016-08-16 09:20:41 +0900

    vs code를 이용한 커밋

--- source/Serial_ui.ui.py
+++ source/Serial_ui.ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Serial_ui.ui'
 #
-# Created: Thu Aug 04 11:16:29 2016
+# Created: Fri Aug 05 09:40:11 2016
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
